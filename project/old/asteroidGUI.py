5514b39e4e3858cc91dbdcdc90466fa3aff82072
raffi <raffi@jubrael.com>
2023-07-01 17:23:40 -0400

    moved old to old folder

--- project/old/asteroidGUI.py
+++ project/old/asteroidGUI.py
@@ -1,25 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
-import backend
+import urllib.request
+import io
+from PIL import ImageTk, Image
 
 #   Need to specify a title for the application window
 #   Add title to application
 #   Specify function for search functionality
 #   Add functionality to automatically pull asteroid list on application startup
 
-DATABASE = {}
-
-
 
 #Create the main window as index and set default window size
 index = tk.Tk()
-index.title("Near Earth Objects Viewer")
+index.title("**APPLICATION TITLE**")
 index.geometry("1000x600")
 index.resizable(False, False)
 index.columnconfigure(1, weight=1)
 
 #Create a new label to contain a title
-main_title = ttk.Label(index, text="Near Earth Objects Viewer")
+main_title = ttk.Label(index, text="**ASTEROID TRACKING TITLE HERE**")
 main_title.grid(row=0, column=1, pady=30)
 main_title.config(font=(24))
 
@@ -29,38 +28,40 @@ search_frame.grid(row=1, column=1, sticky="SW")
 
 #Create a Frame to house asteroid list
 asteroid_list_fame = ttk.Frame(index)
-asteroid_list_fame.grid(row=2, column=0, columnspan=2)
+asteroid_list_fame.grid(row=2, column=0, columnspan=2, ipady=75)
 
 #Frame to house logs
 log_frame = ttk.Frame(index)
-log_frame.grid(row=3, column=0, columnspan=2)
+log_frame.grid(row=3, column=0, columnspan=2, ipady=55)
 
 #Create a frame to house buttons
 button_frame = ttk.Frame(index)
 button_frame.grid(row=4, column=0, columnspan=2)
 
-# image link https://th.bing.com/th?id=OIF.d%2fCjgd%2fJ42C1VjiLa2RSXg&pid=ImgDet&rs=1
-img = tk.PhotoImage(file="asteroid.gif")
+#Add an image
+image_link = "https://th.bing.com/th?id=OIF.d%2fCjgd%2fJ42C1VjiLa2RSXg&pid=ImgDet&rs=1"
+
+class WebImage:
+    def __init__(self, url):
+        with urllib.request.urlopen(url) as u:
+            raw_data = u.read()
+        image = Image.open(io.BytesIO(raw_data)).resize((260, 130))
+        self.image = ImageTk.PhotoImage(image)
+
+    def get(self):
+        return self.image
+
+img = WebImage(image_link).get()
 imagelab = ttk.Label(index, image=img)
 imagelab.grid(row=0, column=0, rowspan=2)
 
 #Asteroid List
 ttk.Label(asteroid_list_fame, text="This is where our asteroid list will go from JSON dump",
-          background="grey", width=100).grid(row=0,column=0,sticky="N")
-asteroid_columns = ("Column1", "Column2", "Column3", "Column4", "We can add more..")
-ast_tree = ttk.Treeview(asteroid_list_fame, columns=asteroid_columns, show='headings')
-ast_tree.grid(row=1, column=0)
-for col in asteroid_columns:
-    ast_tree.heading(col, text=col)
+          background="grey", width=150).grid(sticky="N")
 
 #Log List
 ttk.Label(log_frame, text="This is where our logs will be",
-          background="black", foreground="white", width=100).grid(row=0,column=0, sticky="N")
-log_columns = ("Column1", "Column2", "Column3", "Column4", "We can add more..")
-log_tree = ttk.Treeview(log_frame, columns=log_columns, show='headings')
-log_tree.grid(row=1, column=0)
-for col in log_columns:
-    log_tree.heading(col, text=col)
+          background="black", foreground="white", width=150).grid(sticky="N")
 
 #Asteroid search bar
 asteroid_name = tk.StringVar()
