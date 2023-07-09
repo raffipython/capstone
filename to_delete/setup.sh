cc8bb4a8f0df4de97dca047e465202a9b87767e1
raffi <raffi@jubrael.com>
2023-07-09 06:36:03 -0400

    Deleted old files after copying it to shared folder on google drive

--- to_delete/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Using a Debian Linux host
-sudo apt-get -y install terminator docker docker-compose
-sudo docker pull debian:buster
-sudo docker build -t capstone .
-
-pip install -U cmd2
-
