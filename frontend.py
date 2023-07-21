5b4d4eb3d6f5b8fbcbc3962e9e544c55234d852a
raffi <raffi@jubrael.com>
2023-07-21 14:55:06 -0400

    Updated documentation and merged changes

--- frontend.py
+++ frontend.py
@@ -9,25 +9,68 @@ import sys
 import tkinter as tk
 from tkinter import ttk
 import backend as be
+import logging
+import datetime as dt
 
 
-def asteroid_data_handler():
+def on_item_click(event):
+    """ Gets an event object from GUI, and it calls the asteroid data handler function to only show one asteroid
+    :param event: tkinter event object from the GUI to indicate which item was clicked on
+    :type event: tkinter.Event
+    :return: None
+    """
+    item = ast_tree.selection()[0]
+    item_text = ast_tree.item(item, 'text')
+    msg = f"{actions.get(5)} {item_text} was clicked, GUI event {event}"
+    logging.warning(msg)
+    asteroid_data_handler(item_text)
+
+
+def asteroid_data_handler(name="", clear=False):
     """ Gets data from the search text field and searches the database for that asteroid.
     If it is the first time calling this function, it populates the whole list of NEOs in the GUI
     :return: None
     """
 
-    text = asteroid_name_entry.get()
+    if name:
+        text = name
+    else:
+        text = asteroid_name_entry.get()
     if text and len(ast_tree.get_children()) > 0:
         name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date = be.asteroid(db, text, count)
         for item in ast_tree.get_children():
             ast_tree.delete(item)
         ast_tree.insert('',
                         'end',
-                        text="",
+                        text=name,
                         values=(name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date)
                         )
+        # Log SEARCH type and data on the asteroid
+        msg = f"{actions.get(3)} {name}, {dist_min}, {dist_min_miles}, {threat}, {velocity}, {ca_date}, {impact_date}"
+        logging.warning(msg)
 
+        # Define dtg_value and event_type_value for logging
+        dtg_value = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        event_type_value = actions.get(3)  # This gets the event TYPE for SEARCH action
+
+        # Log the same data to the log_tree
+        log_tree.insert('',
+                        'end',
+                        values=(name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date, dtg_value,
+                                event_type_value)
+                        )
+    elif not clear:
+        for item in ast_tree.get_children():
+            ast_tree.delete(item)
+        for i in range(count):
+            name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date = be.asteroid(db,
+                                                                                                 db['data'][i][0],
+                                                                                                 count)
+            ast_tree.insert('',
+                            'end',
+                            text=name,
+                            values=(name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date)
+                            )
     else:
         if len(ast_tree.get_children()) == 0:
             for i in range(count):
@@ -36,7 +79,7 @@ def asteroid_data_handler():
                                                                                                      count)
                 ast_tree.insert('',
                                 'end',
-                                text="",
+                                text=name,
                                 values=(name, dist_min, dist_min_miles, threat, velocity, ca_date, impact_date)
                                 )
 
@@ -48,7 +91,7 @@ count = 0
 # Create the main window as index and set default window size
 index = tk.Tk()
 index.title("Near Earth Objects Viewer")
-index.geometry("1080x700")
+index.geometry("1400x700")
 index.resizable(False, False)
 index.columnconfigure(1, weight=1)
 
@@ -86,6 +129,7 @@ asteroid_columns = ("Designation", "Distance (AU)", "Distance (Miles)", "Threat"
                     "Date if trajectory changed")
 ast_tree = ttk.Treeview(asteroid_list_fame, columns=asteroid_columns, show='headings')
 ast_tree.grid(row=1, column=0)
+ast_tree.bind("<<TreeviewSelect>>", on_item_click)
 
 for col in asteroid_columns:
     ast_tree.heading(col, text=col)
@@ -94,11 +138,14 @@ for col in asteroid_columns:
 # Log List
 ttk.Label(log_frame, text="This is where our logs will be",
           background="black", foreground="white", width=100).grid(row=0, column=0, sticky="N")
-log_columns = ("Column1", "Column2", "Column3", "Add more if need be...")
+log_columns = ("Asteroid", "Au Distance", "Distance Miles", "threat", "Velocity", "Closest approach", "Impact date",
+               "DTG", "Event Type")
 log_tree = ttk.Treeview(log_frame, columns=log_columns, show='headings')
 log_tree.grid(row=1, column=0)
-for col in log_columns:
+column_widths = [100, 80, 100, 60, 70, 140, 120, 140, 100]
+for col, width in zip(log_columns, column_widths):
     log_tree.heading(col, text=col)
+    log_tree.column(col, anchor="center", stretch=True, width=width)
 
 # Asteroid search bar
 asteroid_name = tk.StringVar()
@@ -129,9 +176,25 @@ if __name__ == '__main__':
     api_data = be.neos_approaching(DAYS)
     db = json.loads(api_data.content)
     count = db['count']
+
+    # Logging configurations
+    FORMAT = '%(asctime)s - %(message)s'
+    current_date = dt.datetime.now().strftime("%Y-%b-%d")
+    # I had to add my desktop filepath in, but it worked.
+    LOG_FILENAME = f"{current_date}.txt"
+    logging.basicConfig(filename=LOG_FILENAME,
+                        level=logging.WARNING,
+                        format='%(asctime)s - %(message)s - %(levelname)s',
+                        datefmt='%Y-%b-%d %H:%M:%S')
+
+    actions = {1: "START", 2: "STOP", 3: "SEARCH", 4: "ERROR", 5: "GUI EVENT"}
+    logging.warning(actions.get(1))
+
     if api_data.status_code == 200:
-        asteroid_data_handler()
+        asteroid_data_handler(clear=True)
         index.mainloop()
+        logging.warning(actions.get(2))
     else:
         print("Something wrong, could not fetch data. Exiting.")
+        logging.warning(actions.get(4))
         sys.exit(1)
